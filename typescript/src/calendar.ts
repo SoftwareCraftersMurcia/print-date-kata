ff9ca5229eb8910b633cd4ea286c407b881b61a2
Pascual Cano <pascual@allhuman.com>
2023-02-24 18:58:22 +0100

    Test with a stup on the calendar

--- typescript/src/calendar.ts
+++ typescript/src/calendar.ts
@@ -1,4 +1,4 @@
-class Calendar {
+export class Calendar {
   today(): Date {
     return new Date();
   }
--- typescript/tests/print_date.spec.ts
+++ typescript/tests/print_date.spec.ts
@@ -2,11 +2,21 @@ import PrintDate from "../src/print_date";
 import Calendar from "../src/calendar";
 import Printer from "../src/printer";
 
-describe("Test suite", () => {
-  it("find a proper name for this test", () => {
+describe("Print date behaviour", () => {
+  it("date is printed on the console", () => {
     const calendar = new Calendar();
     const printer = new Printer();
     const printDate = new PrintDate(calendar, printer);
     printDate.printCurrentDate();
+  });
+
+  it("check that Calendar today is called", () => {
+    const spy = jest.spyOn(Calendar.prototype, "today");
+    spy.mockReturnValue(new Date("2020-01-01"));
+    const calendar = new Calendar();
+    const printer = new Printer();
+    const printDate = new PrintDate(calendar, printer);
+    printDate.printCurrentDate();
+    expect(spy).toHaveBeenCalled();
   });
 });
